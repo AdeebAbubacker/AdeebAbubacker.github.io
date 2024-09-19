effcce2ae71634424421f10c454aaf94db001342
Adeeb Abubacker <148857537+adeebca@users.noreply.github.com>
2024-09-19 21:11:59 +0530

    initial commmit

--- src/assets/index.js
+++ src/assets/index.js
@@ -11,7 +11,6 @@ import css from "./tech/css.png";
 import flutter from "./tech/flutter.png";
 import contentCreator from "./content creator.png";
 import docker from "./tech/docker.png";
-import figma from "./tech/figma.png";
 import git from "./tech/git.png";
 import html from "./tech/html.png";
 import javascript from "./tech/javascript.png";
@@ -47,7 +46,6 @@ export {
   close,
   css,
   docker,
-  figma,
   firebase,
   git,
   html,
